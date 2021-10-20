3a9b5c7557f82a34d22e5f08ebd7ed5756f4463b
Jesse Cota <jesse.cota@gmail.com>
2021-10-20 18:50:05 -0300

    Add vpc module

---
+++ modules.tf
@@ -0,0 +1,18 @@
+# vpc module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.10.0"
+ 
+  name = "${var.project}-${var.env}"
+  cidr = "10.0.0.0/16"
+
+  create_database_subnet_group = true
+
+  azs               = ["us-east-1a", "us-east-1b"]
+  public_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets  = ["10.0.201.0/24", "10.0.202.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+ }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  default = "default"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "project" {
+  default = "jaylabs"
+}
+
+variable "env" {
+  default = "dev"
+}
