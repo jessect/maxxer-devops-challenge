95b1506807eeb482d5e218d92ace7fa6496e18d9
Jesse Cota <jesse.cota@gmail.com>
2021-10-21 22:02:55 -0300

    Update variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,15 +1,49 @@
 variable "profile" {
-  default = "default"
+  description = "Profile configured on ~/.aws/credentials"
+  default     = "default"
 }
 
 variable "region" {
-  default = "us-east-1"
+  description = "Region name"
+  default     = "us-east-1"
 }
 
 variable "project" {
-  default = "jaylabs"
+  description = "Project name"
+  default     = "jaylabs"
 }
 
 variable "env" {
-  default = "dev"
+  description = "Environment name"
+  default     = "dev"
+}
+
+variable "repo_name" {
+  description = "Name of the codecommit repository."
+  default     = "myapp"
+}
+
+variable "repo_default_branch" {
+  description = "The name of the default repository branch"
+  default     = "develop"
+}
+
+variable "force_artifact_destroy" {
+  description = "Force the removal of the artifact S3 bucket on destroy"
+  default     = "true"
+}
+
+variable "build_timeout" {
+  description = "The time to wait for a CodeBuild to complete before timing out in minutes"
+  default     = "5"
+}
+
+variable "build_compute_type" {
+  description = "The build instance type for CodeBuild"
+  default     = "BUILD_GENERAL1_SMALL"
+}
+
+variable "build_image" {
+  description = "The build image for CodeBuild to use"
+  default     = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
 }
